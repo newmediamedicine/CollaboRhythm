978cee8deb3d41d5b24b6fb3b5863a3321479d5b
John Moore <jom@mit.edu>
2012-04-05 12:11:03 -0400

    Updated the DeviceGateway so that both blood pressure and blood glucose are supported on the ForaD40b. Also moved all code associated with this device to a single class called ForaD40bBluetoothSocketThread. Now the DeviceGatewayService is extensible in that other classes that implement IBluetoothSocketThread can be registered with it by adding their class names to the mBluetoothSocketThreadNames array. Each bluetoothSocketThread is responsible for handling the connection with a particular device, retrieving the data, and returning it to the DeviceGateway to be communicated to CollaboRhythm. Each class must provide a BluetoothSocketThreadAnnotation that specifies the name of the Bluetooth device that it handles.

--- CollaboRhythm.Android.DeviceGateway/src/collaboRhythm/android/deviceGateway/DeviceGatewayService.java
+++ CollaboRhythm.Android.DeviceGateway/src/collaboRhythm/android/deviceGateway/DeviceGatewayService.java
@@ -26,7 +26,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
-import android.text.format.DateFormat;
 import android.widget.Toast;
 
 import com.google.code.microlog4android.Logger;
@@ -37,214 +36,202 @@ import com.google.code.microlog4android.appender.SyslogAppender;
 
 import java.util.Date;
 
-public class DeviceGatewayService extends Service
-{
+public class DeviceGatewayService extends Service {
 
-    private static final String TAG = "CollaboRhythm.Android.DeviceGateway";
-    private static final String CLASS = "DeviceGatewayService";
+	private static final String TAG = "CollaboRhythm.Android.DeviceGateway";
+	private static final String CLASS = "DeviceGatewayService";
 
-    private final static Logger log = LoggerFactory.getLogger();
+	private final static Logger log = LoggerFactory.getLogger();
 
-    private BluetoothAdapter mBluetoothAdapter;
-    private Boolean mBluetoothSupported;
+	public static final int RETRIEVE_DATA_SUCCEEDED = 3;
+	public static final int RETRIEVE_DATA_FAILED = 4;
 
-    private BluetoothServerSocketThread mBluetoothServerSocketThread;
-    private Boolean mBluetoothServerSocketRunning = false;
+	private BluetoothAdapter mBluetoothAdapter;
+	private Boolean mBluetoothSupported;
 
-    private BluetoothDevice mBluetoothDevice;
+	private BluetoothServerSocketThread mBluetoothServerSocketThread;
+	private Boolean mBluetoothServerSocketRunning = false;
 
-    @Override
-    public IBinder onBind(Intent arg0)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	private BluetoothDevice mBluetoothDevice;
 
-    @Override
-    public void onCreate()
-    {
-        initializeLogging();
+	// The DeviceGatewayService is extensible in that it can support connections from an arbitrary number of Bluetooth devices.
+	// A separate class that extends Thread and implements IBluetoothSocketThread must be created for each device. The class must
+	// provide a BluetoothSocketThreadAnnotation that specifies the name of the Bluetooth device that it handles. The name
+	// of each class must then be added to this array.
+	private String mBluetoothSocketThreadNames[] = {"collaboRhythm.android.deviceGateway.ForaD40bBluetoothSocketThread"};
 
-        log.debug(CLASS + ": onCreate");
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null)
-        {
-            log.debug(CLASS + ": Bluetooth not supported.");
-            createToast("Bluetooth not supported.");
-            mBluetoothSupported = false;
-        } else
-        {
-            mBluetoothSupported = true;
-        }
-    }
+	@Override
+	public void onCreate() {
+		initializeLogging();
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        log.debug(CLASS + ": onStart");
+		log.debug(CLASS + ": onCreate");
 
-        setForeground(true);
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			log.debug(CLASS + ": Bluetooth not supported.");
+			createToast("Bluetooth not supported.");
+			mBluetoothSupported = false;
+		} else {
+			mBluetoothSupported = true;
+		}
+	}
 
-        if (mBluetoothSupported)
-        {
-            Bundle extras = intent.getExtras();
-            if (extras != null && extras.getBoolean("bluetoothDeviceDisconnected"))
-            {
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		log.debug(CLASS + ": onStart");
+
+		setForeground(true);
+
+		if (mBluetoothSupported) {
+			Bundle extras = intent.getExtras();
+			if (extras != null && extras.getBoolean("bluetoothDeviceDisconnected")) {
 				mBluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                startBluetoothDeviceConnectThread();
-            }
-            else
-            {
-                if (mBluetoothAdapter.isEnabled())
-                {
-                    startBluetoothServerSocketThread();
-                } else
-                {
-                    createToast("Please do not turn off Bluetooth.");
-                    stopBluetoothServerSocketThread();
-                    mBluetoothAdapter.enable();
-                }
-            }
-        }
+				startBluetoothDeviceConnectThread();
+			} else {
+				if (mBluetoothAdapter.isEnabled()) {
+					startBluetoothServerSocketThread();
+				} else {
+					createToast("Please do not turn off Bluetooth.");
+					stopBluetoothServerSocketThread();
+					mBluetoothAdapter.enable();
+				}
+			}
+		}
 
-        // Continue running until it is explicitly stopped
-        return START_STICKY;
-    }
+		// Continue running until it is explicitly stopped
+		return START_STICKY;
+	}
 
-    @Override
-    public void onDestroy()
-    {
-        log.debug(CLASS + ": onDestroy");
-    }
+	@Override
+	public void onDestroy() {
+		log.debug(CLASS + ": onDestroy");
+	}
 
-    private void startBluetoothServerSocketThread()
-    {
-        if (!mBluetoothServerSocketRunning)
-        {
-            mBluetoothServerSocketThread = new BluetoothServerSocketThread(mServiceMessageHandler);
-            if (mBluetoothServerSocketThread.mmBluetoothServerSocket != null)
-            {
-                mBluetoothServerSocketThread.start();
-                mBluetoothServerSocketRunning = true;
-            } else
-            {
-                mBluetoothAdapter.disable();
-            }
-        }
-    }
+	private void startBluetoothServerSocketThread() {
+		if (!mBluetoothServerSocketRunning) {
+			mBluetoothServerSocketThread = new BluetoothServerSocketThread(mServiceMessageHandler);
+			if (mBluetoothServerSocketThread.mmBluetoothServerSocket != null) {
+				mBluetoothServerSocketThread.start();
+				mBluetoothServerSocketRunning = true;
+			} else {
+				mBluetoothAdapter.disable();
+			}
+		}
+	}
 
-    private void stopBluetoothServerSocketThread()
-    {
-        if (mBluetoothServerSocketThread != null && mBluetoothServerSocketRunning)
-        {
-            mBluetoothServerSocketThread.cancel();
-            mBluetoothServerSocketRunning = false;
-        }
-    }
+	private void stopBluetoothServerSocketThread() {
+		if (mBluetoothServerSocketThread != null && mBluetoothServerSocketRunning) {
+			mBluetoothServerSocketThread.cancel();
+			mBluetoothServerSocketRunning = false;
+		}
+	}
 
-    private void startBluetoothSocketThread(BluetoothSocket bluetoothSocket)
-    {
-        BluetoothSocketThread bluetoothSocketThread = new BluetoothSocketThread(bluetoothSocket,
-                                                                                mServiceMessageHandler);
-        if (bluetoothSocketThread.mmInStream != null && bluetoothSocketThread.mmOutStream != null)
-        {
-            bluetoothSocketThread.start();
-        } else
-        {
-            instructUserToRetry();
-        }
-    }
+	private void startBluetoothSocketThread(BluetoothSocket bluetoothSocket) {
+		BluetoothDevice bluetoothDevice = bluetoothSocket.getRemoteDevice();
 
-    private void startBluetoothDeviceConnectThread()
-    {
-        BluetoothDeviceConnectThread bluetoothDeviceConnectThread = new BluetoothDeviceConnectThread(mBluetoothDevice,
-                                                                                                     mServiceMessageHandler);
-        if (bluetoothDeviceConnectThread.mmBluetoothSocket != null)
-        {
-            bluetoothDeviceConnectThread.start();
-        } else
-        {
-            instructUserToRetry();
-        }
-    }
+		for (String bluetoothSocketThreadName : mBluetoothSocketThreadNames) {
+			try {
+				BluetoothSocketThreadAnnotation bluetoothSocketThreadAnnotation = Class.forName(bluetoothSocketThreadName).getAnnotation(BluetoothSocketThreadAnnotation.class);
+				if (bluetoothDevice.getName().equals(bluetoothSocketThreadAnnotation.bluetoothDeviceName())) {
+					Class bluetoothSocketThreadClass = Class.forName(bluetoothSocketThreadName);
+					IBluetoothSocketThread bluetoothSocketThread = (IBluetoothSocketThread) bluetoothSocketThreadClass.newInstance();
+					bluetoothSocketThread.init(bluetoothSocket, mServiceMessageHandler);
+					if (bluetoothSocketThread.isThreadReady()) {
+						bluetoothSocketThread.start();
+					} else {
+						instructUserToRetry();
+					}
+				}
+			} catch (ClassNotFoundException e) {
+				log.debug(CLASS + ": Class not found for bluetoothSocketThreadName " + bluetoothSocketThreadName + " in mBluetoothSocketThreadNames.");
+			} catch (InstantiationException e) {
+				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+			}
+		}
+	}
 
-    private void instructUserToRetry()
-    {
-        Intent startCollaboRhythmIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("collaborhythm://retry=true"));
-        startCollaboRhythmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(startCollaboRhythmIntent);
-    }
+	private void startBluetoothDeviceConnectThread() {
+		BluetoothDeviceConnectThread bluetoothDeviceConnectThread = new BluetoothDeviceConnectThread(mBluetoothDevice,
+				mServiceMessageHandler);
+		if (bluetoothDeviceConnectThread.mmBluetoothSocket != null) {
+			bluetoothDeviceConnectThread.start();
+		} else {
+			instructUserToRetry();
+		}
+	}
 
-    private void startCollaboRhythm(Bundle data)
-    {
-        String bloodPressureString = "healthActionType=Equipment&healthActionName=Blood Glucose&equipmentName=FORA D40b&success=true&bloodGlucose=" + Integer.toString(
-                data.getInt("systolic")) + "&diastolic=" + Integer.toString(
-                data.getInt("diastolic")) + "&heartrate=" + Integer.toString(data.getInt("heartrate")) + "&date=" + new Date().toGMTString();
+	private void instructUserToRetry() {
+		Intent startCollaboRhythmIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("collaborhythm://retry=true"));
+		startCollaboRhythmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(startCollaboRhythmIntent);
+	}
 
-        Intent startCollaboRhythmIntent = new Intent(Intent.ACTION_VIEW,
-                                                     Uri.parse("collaborhythm://" + bloodPressureString));
-        startCollaboRhythmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	private void startCollaboRhythm(Bundle data) {
+		String intentString = "healthActionType=Equipment&healthActionName=" + data.getString("healthActionName") + "&equipmentName=" + data.getString("equipmentName") + "&success=true&" + data.getString("result") + "&date=" + new Date().toGMTString();
+
+		Intent startCollaboRhythmIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("collaborhythm://" + intentString));
+		startCollaboRhythmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
 //		startCollaboRhythmIntent.setClassName("air.CollaboRhythm.Mobile.debug", "air.CollaboRhythm.Mobile.debug.AppEntry");
-        startActivity(startCollaboRhythmIntent);
-    }
+		startActivity(startCollaboRhythmIntent);
+	}
 
-    public Handler mServiceMessageHandler = new Handler()
-    {
-        //@Override
-        public void handleMessage(Message message)
-        {
-            switch (message.what)
-            {
-                case BluetoothServerSocketThread.BLUETOOTH_DEVICE_CONNECTED:
-                    BluetoothSocket bluetoothSocket = (BluetoothSocket) message.obj;
-                    mBluetoothDevice = bluetoothSocket.getRemoteDevice();
-                    startBluetoothSocketThread(bluetoothSocket);
-                    break;
-                case BluetoothServerSocketThread.BLUETOOTH_SERVER_SOCKET_EXCEPTION:
-                    mBluetoothServerSocketRunning = false;
-                    startBluetoothServerSocketThread();
-                    break;
-                case BluetoothDeviceConnectThread.BLUETOOTH_DEVICE_CONNECT_FAILED:
-                    instructUserToRetry();
-                case BluetoothSocketThread.RETRIEVE_BLOOD_PRESSURE_SUCCEEDED:
-                    startCollaboRhythm(message.getData());
-                    break;
-                case BluetoothSocketThread.RETRIEVE_BLOOD_PRESSURE_FAILED:
-                    if (BluetoothDeviceDisconnectedBroadcastReceiver.mBluetoothDeviceDisconnected && mBluetoothDevice != null)
-                    {
-                        startBluetoothDeviceConnectThread();
-                    } else
-                    {
-                        instructUserToRetry();
-                    }
-                    break;
-            }
-            super.handleMessage(message);
-        }
-    };
+	public Handler mServiceMessageHandler = new Handler() {
+		//@Override
+		public void handleMessage(Message message) {
+			switch (message.what) {
+				case BluetoothServerSocketThread.BLUETOOTH_DEVICE_CONNECTED:
+					BluetoothSocket bluetoothSocket = (BluetoothSocket) message.obj;
+					mBluetoothDevice = bluetoothSocket.getRemoteDevice();
+					startBluetoothSocketThread(bluetoothSocket);
+					break;
+				case BluetoothServerSocketThread.BLUETOOTH_SERVER_SOCKET_EXCEPTION:
+					mBluetoothServerSocketRunning = false;
+					startBluetoothServerSocketThread();
+					break;
+				case BluetoothDeviceConnectThread.BLUETOOTH_DEVICE_CONNECT_FAILED:
+					instructUserToRetry();
+				case RETRIEVE_DATA_SUCCEEDED:
+					startCollaboRhythm(message.getData());
+					break;
+				case RETRIEVE_DATA_FAILED:
+					if (BluetoothDeviceDisconnectedBroadcastReceiver.mBluetoothDeviceDisconnected && mBluetoothDevice != null) {
+						startBluetoothDeviceConnectThread();
+					} else {
+						instructUserToRetry();
+					}
+					break;
+			}
+			super.handleMessage(message);
+		}
+	};
 
-    private void initializeLogging()
-    {
-        // Log the output to LogCat, a Syslog server, and a file
-        LogCatAppender logCatAppender = new LogCatAppender();
-        log.addAppender(logCatAppender);
-        SyslogAppender syslogAppender = new SyslogAppender();
-        syslogAppender.setProperty("host", "18.85.55.246");
-        syslogAppender.setHeader(false);
-        log.addAppender(syslogAppender);
-        FileAppender fileAppender = new FileAppender();
-        fileAppender.setAppend(true);
-        fileAppender.setFileName("DeviceGateway.txt");
-        log.setClientID(TAG);
-        log.addAppender(fileAppender);
-    }
+	private void initializeLogging() {
+		// Log the output to LogCat, a Syslog server, and a file
+		LogCatAppender logCatAppender = new LogCatAppender();
+		log.addAppender(logCatAppender);
+		SyslogAppender syslogAppender = new SyslogAppender();
+		syslogAppender.setProperty("host", "18.85.55.246");
+		syslogAppender.setHeader(false);
+		log.addAppender(syslogAppender);
+		FileAppender fileAppender = new FileAppender();
+		fileAppender.setAppend(true);
+		fileAppender.setFileName("DeviceGateway.txt");
+		log.setClientID(TAG);
+		log.addAppender(fileAppender);
+	}
 
-    private void createToast(CharSequence text)
-    {
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(this, text, duration);
-        toast.show();
-    }
+	private void createToast(CharSequence text) {
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(this, text, duration);
+		toast.show();
+	}
 }
