594ff270ced71bfd98e7f76bdbd485ed2f240f25
John Moore <jom@mit.edu>
2013-06-27 17:31:38 -0400

    Update the version number for CollaboRhythm to 0.8.0

--- CollaboRhythm.Android.DeviceGateway/gen/collaboRhythm/android/deviceGateway/Manifest.java
+++ CollaboRhythm.Android.DeviceGateway/gen/collaboRhythm/android/deviceGateway/Manifest.java
@@ -1,5 +1,3 @@
-/*___Generated_by_IDEA___*/
-
 package collaboRhythm.android.deviceGateway;
 
 /* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
--- CollaboRhythm.NativeExtensions.PlayVideo.Android/gen/collaboRhythm/nativeExtensions/playVideo/android/Manifest.java
+++ CollaboRhythm.NativeExtensions.PlayVideo.Android/gen/collaboRhythm/nativeExtensions/playVideo/android/Manifest.java
@@ -1,5 +1,3 @@
-/*___Generated_by_IDEA___*/
-
 package collaboRhythm.nativeExtensions.playVideo.android;
 
 /* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
